b5539b2711c2e4735584cca9be67fa8dcc4baf3c
Schoepplenberg <zeldaaa@protonmail.com>
2019-04-20 19:13:02 +0200

    JavaFX GUI

---
+++ src/sample/Controller.java
@@ -0,0 +1,60 @@
+package sample;
+
+import com.lynden.gmapsfx.GoogleMapView;
+import com.lynden.gmapsfx.MapComponentInitializedListener;
+import com.lynden.gmapsfx.javascript.object.*;
+import com.lynden.gmapsfx.service.geocoding.GeocoderStatus;
+import com.lynden.gmapsfx.service.geocoding.GeocodingResult;
+import com.lynden.gmapsfx.service.geocoding.GeocodingService;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+
+public class Controller implements Initializable, MapComponentInitializedListener {
+
+
+    @FXML
+    private GoogleMapView mapView;
+
+    @FXML
+    private TextField addressTextField;
+
+    private GoogleMap map;
+
+    private GeocodingService geocodingService;
+
+    private StringProperty address = new SimpleStringProperty();
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        mapView.addMapInializedListener(this);
+        //address.bind(addressTextField.textProperty());
+    }
+
+    @Override
+    public void mapInitialized() {
+        geocodingService = new GeocodingService();
+        MapOptions mapOptions = new MapOptions();
+
+        mapOptions.center(new LatLong(53.527206, 9.918959))
+                .mapType(MapTypeIdEnum.ROADMAP)
+                .overviewMapControl(false)
+                .panControl(false)
+                .rotateControl(false)
+                .scaleControl(false)
+                .streetViewControl(false)
+                .zoomControl(false)
+                .zoom(10);
+
+        map = mapView.createMap(mapOptions);
+
+    }
+
+}
